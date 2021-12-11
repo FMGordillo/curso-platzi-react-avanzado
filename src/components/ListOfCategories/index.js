207d29f7c0a4cb93ead6d9955f306242484b23df
FMGordillo <facundomgordillo@gmail.com>
2021-12-11 17:15:05 -0300

    feature: added fetch from server for categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, ListItem } from './styles'
 
 import data from '../../../api/db.json'
 
 export function ListOfCategories () {
-  const { categories } = data || {}
+  const [categories, setCategories] = useState(data.categories)
+
+  useEffect(() => {
+    // TODO: Make this an env var
+    window.fetch('https://curso-platzi-react-avanzado-api.vercel.app/categories')
+      .then(data => data.json())
+      .then(data => setCategories(data))
+  }, [setCategories])
 
   return (
     <List>
